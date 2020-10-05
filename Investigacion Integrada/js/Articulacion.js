23ea7b2365e4a5eb313699bd754165ecd96f55b7
Lord-Jony <62217008+Jony-07@users.noreply.github.com>
2020-10-04 23:59:03 -0600

    Penultimo

--- Investigacion Integrada/js/Articulacion.js
+++ Investigacion Integrada/js/Articulacion.js
@@ -1,62 +1,99 @@
+RegExpTel = /^\+(5)\d{1,3}\ ?\-?\d{8}$/;
+RegExpEmail = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+RegExpLastNameandName = /^[A-Z a-záéúíóñ]{4,30}$/;
 
-function InvalidName(name) {
-
-    if(name.validity.patternMismatch){
-       name.setCustomValidity('Campo no númerico, minímo 4 letras');
-       
-   }    
-   else {
-    name.setCustomValidity('');
-   }
-   return true;
-}
-
-function InvalidLastName(name) {
-
-    if(name.validity.patternMismatch){
-       name.setCustomValidity('Campo no númerico, minímo 4 letras');
-       
-   }    
-   else {
-    name.setCustomValidity('');
-   }
-   return true;
-}
-
-
-
-//
-function registrar()
+RegExpLastName = /^[A-Z a-záéúíóñ]{4,30}$/;
+//Anula submit
+/*document.getElementById('frm').addEventListener('submit', function(e){
+    e.preventDefault();
+    e.e
+});*/
+//Valida submit
+/*document.getElementById('frm')
 {
-    document.getElementById('frm').addEventListener('submit', function(e){
-        e.preventDefault();
-        e.pre
-    });
-  /*  telefono =document.getElementById('telefono').value;
-      RegExpTel =/^\+(5)\d{1,3}\ ?\-?\d{8}$/
+    document.forms['frm'].submit();  
+}
+*/
+document.getElementById("frm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  e.e;
+});
 
-    if(RegExpTel.test(telefono).validity)
+function registrar() {
+  Telefono = document.getElementById("telefono").value;
+  Email = document.getElementById("email").value;
+  Apellido = document.getElementById("apellido").value;
+  Nombre = document.getElementById("name").value;
+
+  if (
+    !RegExpTel.test(Telefono) ||
+    !RegExpEmail.test(Email) ||
+    !RegExpLastNameandName.test(Apellido) ||
+    !RegExpLastName.test(Nombre)
+  ) {
+    document.getElementById("labelinvisible").style.visibility = "visible";
+    document.getElementById("labelinvisible_correo").style.visibility =
+      "visible";
+    document.getElementById("labelinvisible_lastname").style.visibility =
+      "visible";
+    document.getElementById("labelinvisible_name").style.visibility = "visible";
+  } else {
+    document.getElementById("labelinvisible").style.visibility = "hidden";
+    document.getElementById("labelinvisible_correo").style.visibility =
+      "hidden";
+    document.getElementById("labelinvisible_lastname").style.visibility =
+      "hidden";
+    document.getElementById("labelinvisible_name").style.visibility = "hidden";
+
+    document.getElementById("frm");
     {
-    telefono.setCustomValidity('Campo no númerico, minímo 4 letras');
+      document.forms["frm"].submit();
     }
-    else 
-    {
-        telefono.setCustomValidity('');
-    }*/
-    RegExpTel =/^\+(5)\d{1,3}\ ?\-?\d{8}$/
-   valor = document.getElementById("telefono").value;
-if( !(RegExpTel.test(valor)) ) {
-   
-  alert("False");
-  
-}
+  }
+  /*
+  if (!RegExpTel.test(Telefono)) {
     
+    document.getElementById("labelinvisible").style.visibility = "visible";
+  } else {
+    document.getElementById("labelinvisible").style.visibility = "hidden";
+    if (!RegExpEmail.test(Email)) {
+       
+      document.getElementById('labelinvisible_correo').style.visibility =
+        "visible";
+    } else {
+      document.getElementById('labelinvisible_correo').style.visibility =
+        "hidden";
+        if(!RegExpLastNameandName.test(Apellido))
+        {
+            document.getElementById("labelinvisible_lastname").style.visibility =
+            "visible";
+            
+        }
+        else
+        {
+             
+            document.getElementById("labelinvisible_lastname").style.visibility =
+            "hidden";
+            if(!RegExpLastNameandName.test(Nombre))
+            {
+                document.getElementById("labelinvisible_name").style.visibility =
+                "visible";
+                
+            }
+            else
+            {
+                 
+                document.getElementById("labelinvisible_name").style.visibility =
+                "hidden";
+                document.getElementById('frm')
+{
+    document.forms['frm'].submit();  
 }
-  
-
-
-
-
-
-
-
+                
+            }
+        }
+        
+    }
+  }
+  */
+}
